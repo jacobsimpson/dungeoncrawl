89015bd530779cc2020c9629864f5509e07e1bae
Jacob Simpson <jacob.simpson@gmail.com>
2021-03-14 15:35:59 -0700

    Health HUD and enemy tooltips.

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -31,17 +31,5 @@ pub fn player_input(
             ));
         });
         *turn_state = TurnState::PlayerTurn;
-
-        //if delta.x != 0 || delta.y != 0 {
-        //    let mut players = <&mut Point>::query().filter(component::<Player>());
-        //    players.iter_mut(ecs).for_each(|pos| {
-        //        let destination = *pos + delta;
-        //        if map.can_enter_tile(destination) {
-        //            *pos = destination;
-        //            camera.on_player_move(destination);
-        //            *turn_state = TurnState::PlayerTurn;
-        //        }
-        //    });
-        //}
     }
 }
